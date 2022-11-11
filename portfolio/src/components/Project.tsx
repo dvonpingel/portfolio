0dceb3422fc2a0f47050b4cf046c74422ed3b0dc
D'Artagnan von Pingel <dvonpingel@gmail.com>
2022-11-10 19:57:11 -0700

    Finish typescript conversion and add type packages

--- portfolio/src/components/Project.tsx
+++ portfolio/src/components/Project.tsx
@@ -1,39 +1,55 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
-const Project = ({ title, date, srcHref, projectHref, delay, explanation }) => {
+interface ProjectProps {
+  title: string
+  date: string
+  srcHref?: string
+  projectHref?: string
+  delay: string
+  explanation: React.ReactNode
+}
+
+const Project = ({
+  title,
+  date,
+  srcHref,
+  projectHref,
+  delay,
+  explanation,
+}: ProjectProps) => {
   const Paragraph = styled.p`
     opacity: 0;
     animation: ${kf} 1s forwards;
     animation-delay: ${delay};
     color: white;
-    font-family: "Cairo", sans-serif;
+    font-family: 'Cairo', sans-serif;
     font-size: 1.6rem;
     line-height: 1.2;
     text-align: center;
     letter-spacing: 0.2px;
     word-spacing: 1px;
-  `;
+  `
   return (
     <Paragraph>
-      •{" "}
+      •{' '}
       {projectHref ? (
-        <Project1 href={projectHref} target="_blank">
+        <Project1 href={projectHref} target='_blank'>
           {title}
         </Project1>
       ) : (
         <AltProjectTitle>{title}</AltProjectTitle>
-      )}{" "}
-      {date}{" "}
+      )}{' '}
+      {date}{' '}
       {srcHref && (
-        <SourceCode href={srcHref} target="_blank">
+        <SourceCode href={srcHref} target='_blank'>
           SOURCE CODE
         </SourceCode>
       )}
       <NoteParagraph>{explanation}</NoteParagraph>
     </Paragraph>
-  );
-};
+  )
+}
 
 const kf = keyframes`
     100% {
@@ -41,26 +57,26 @@ const kf = keyframes`
         transform: scale(1);
         transform: translate(0);
     }
-`;
+`
 
 const Paragraph = styled.p`
   opacity: 0;
   animation: ${kf} 1s forwards;
   animation-delay: 0.2s;
   color: white;
-  font-family: "Cairo", sans-serif;
+  font-family: 'Cairo', sans-serif;
   font-size: 1.6rem;
   line-height: 1.5;
   text-align: center;
   letter-spacing: 0.2px;
   word-spacing: 1px;
-`;
+`
 
 const NoteParagraph = styled(Paragraph)`
   font-size: 1rem;
   width: 475px;
   margin: 10px auto;
-`;
+`
 
 const SourceCode = styled.a`
   font-size: 1rem;
@@ -71,18 +87,18 @@ const SourceCode = styled.a`
   &:hover {
     text-decoration: underline 1px;
   }
-`;
+`
 
 const Project1 = styled.a`
   color: white;
   font-weight: 700;
   text-decoration: solid underline white 2px;
-`;
+`
 
 const AltProjectTitle = styled.div`
   color: white;
   font-weight: 700;
   display: inline;
-`;
+`
 
-export default Project;
+export default Project
